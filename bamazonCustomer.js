d4f7c44ef5c37ec50af541097aa4251d8c9c5773
Marco Angel <angel2367716@gmail.com>
2019-08-26 04:40:50 -0700

    Now the information from the table products can be displayed on the command line

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,14 +13,21 @@ const connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-    afterConnection();
+    createTable();
 });
 
-function afterConnection(){
-    connection.query("SELECT * FROM products", function (err, res){
-        if(err) throw err; 
-        console.log(res);
-        connection.end();
-    })
-}
+const createTable = function () {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        for (let i = 0; i < res.length; i++) {
 
+            console.log(
+                "Item: " + res[i].item_id + " || " +
+                "Product: " + res[i].product_name + " || " +
+                "Department: " + res[i].department_name + " || " +
+                "Price: $" + res[i].price + " || " +
+                "Stock: " + res[i].stock_quantity + "\n");
+        }
+      
+    });
+}
